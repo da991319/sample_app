cc0d79a0711fbea1397ba18f5ef17437e1474278
regis castaing <regis@regis-laptop.(none)>
2011-01-13 07:40:24 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'fcde3b483b7af98b81ac5f360096139cd0e519f257a81f4b311b2683b4187c35caee1f13461a4f2bad0d703284d489c1042e5ec96e1dbf4cda8291be9f51cc4f'
